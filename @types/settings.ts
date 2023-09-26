c51bb384870bb7e0943851edb980383e163fac92
Felix Tellmann <felix@tellmann.co.za>
2023-09-26 12:27:07 +0200

    Shopify Color Themes Upgrade

--- @types/settings.ts
+++ @types/settings.ts
@@ -1,3 +1,3 @@
-import { _Color_liquid, _Font_liquid, _Image_liquid } from "types/shopify";
+import { _Color_liquid, _Font_liquid, _Image_liquid, _BlockTag } from "types/shopify";
 
 export type SettingsSchema = any;
